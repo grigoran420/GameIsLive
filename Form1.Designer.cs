41b4dde9577a36e8a49a6c2fb7ec64fd7a5c283e
P01ntZer0 <grabitel228@outlook.com>
2021-02-23 15:05:47 +0300

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,311 @@
+﻿namespace game_is_live
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.button1 = new System.Windows.Forms.Button();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.игратьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.стартToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.стопToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripSeparator();
+            this.отчиститьПолеToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.выйтиИзИгрыToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.менюToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.игартьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.выходToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.стартToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.стопToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem2 = new System.Windows.Forms.ToolStripSeparator();
+            this.очиститьПолеToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.contextMenuStrip1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork_1);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.ContextMenuStrip = this.contextMenuStrip1;
+            this.progressBar1.Location = new System.Drawing.Point(171, 306);
+            this.progressBar1.MarqueeAnimationSpeed = 1;
+            this.progressBar1.Maximum = 1296;
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(386, 23);
+            this.progressBar1.Step = 1;
+            this.progressBar1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.ContextMenuStrip = this.contextMenuStrip1;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 32F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(195, 359);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(335, 78);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.ContextMenuStrip = this.contextMenuStrip1;
+            this.trackBar1.LargeChange = 1;
+            this.trackBar1.Location = new System.Drawing.Point(171, 797);
+            this.trackBar1.Maximum = 1000;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(386, 45);
+            this.trackBar1.SmallChange = 10;
+            this.trackBar1.TabIndex = 3;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            this.trackBar1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ContextMenuStrip = this.contextMenuStrip1;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(565, 795);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 26);
+            this.label1.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ContextMenuStrip = this.contextMenuStrip1;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label2.Location = new System.Drawing.Point(70, 795);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 26);
+            this.label2.TabIndex = 4;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoEllipsis = true;
+            this.checkBox1.ContextMenuStrip = this.contextMenuStrip1;
+            this.checkBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.checkBox1.Location = new System.Drawing.Point(46, 798);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(24, 20);
+            this.checkBox1.TabIndex = 5;
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            this.checkBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.AllowDrop = true;
+            this.contextMenuStrip1.AllowMerge = false;
+            this.contextMenuStrip1.AutoClose = false;
+            this.contextMenuStrip1.ImeMode = System.Windows.Forms.ImeMode.On;
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.игратьToolStripMenuItem,
+            this.выйтиИзИгрыToolStripMenuItem});
+            this.contextMenuStrip1.LayoutStyle = System.Windows.Forms.ToolStripLayoutStyle.Table;
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(156, 48);
+            // 
+            // игратьToolStripMenuItem
+            // 
+            this.игратьToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.стартToolStripMenuItem,
+            this.стопToolStripMenuItem,
+            this.toolStripMenuItem1,
+            this.отчиститьПолеToolStripMenuItem});
+            this.игратьToolStripMenuItem.Name = "игратьToolStripMenuItem";
+            this.игратьToolStripMenuItem.Size = new System.Drawing.Size(155, 22);
+            this.игратьToolStripMenuItem.Text = "Играть";
+            // 
+            // стартToolStripMenuItem
+            // 
+            this.стартToolStripMenuItem.Name = "стартToolStripMenuItem";
+            this.стартToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.стартToolStripMenuItem.Text = "Старт";
+            this.стартToolStripMenuItem.Click += new System.EventHandler(this.стартToolStripMenuItem_Click);
+            // 
+            // стопToolStripMenuItem
+            // 
+            this.стопToolStripMenuItem.Name = "стопToolStripMenuItem";
+            this.стопToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.стопToolStripMenuItem.Text = "Стоп";
+            this.стопToolStripMenuItem.Click += new System.EventHandler(this.стопToolStripMenuItem_Click);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(177, 6);
+            // 
+            // отчиститьПолеToolStripMenuItem
+            // 
+            this.отчиститьПолеToolStripMenuItem.Name = "отчиститьПолеToolStripMenuItem";
+            this.отчиститьПолеToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.отчиститьПолеToolStripMenuItem.Text = "Отчистить поле";
+            this.отчиститьПолеToolStripMenuItem.Click += new System.EventHandler(this.отчиститьПолеToolStripMenuItem_Click);
+            // 
+            // выйтиИзИгрыToolStripMenuItem
+            // 
+            this.выйтиИзИгрыToolStripMenuItem.Name = "выйтиИзИгрыToolStripMenuItem";
+            this.выйтиИзИгрыToolStripMenuItem.Size = new System.Drawing.Size(155, 22);
+            this.выйтиИзИгрыToolStripMenuItem.Text = "Выйти из игры";
+            this.выйтиИзИгрыToolStripMenuItem.MouseDown += new System.Windows.Forms.MouseEventHandler(this.выйтиИзИгрыToolStripMenuItem_MouseDown);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.AllowDrop = true;
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.менюToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(752, 24);
+            this.menuStrip1.TabIndex = 7;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // менюToolStripMenuItem
+            // 
+            this.менюToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.игартьToolStripMenuItem,
+            this.выходToolStripMenuItem});
+            this.менюToolStripMenuItem.Name = "менюToolStripMenuItem";
+            this.менюToolStripMenuItem.Size = new System.Drawing.Size(53, 20);
+            this.менюToolStripMenuItem.Text = "Меню";
+            // 
+            // игартьToolStripMenuItem
+            // 
+            this.игартьToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.стартToolStripMenuItem1,
+            this.стопToolStripMenuItem1,
+            this.toolStripMenuItem2,
+            this.очиститьПолеToolStripMenuItem});
+            this.игартьToolStripMenuItem.Name = "игартьToolStripMenuItem";
+            this.игартьToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.игартьToolStripMenuItem.Text = "Игарть";
+            // 
+            // выходToolStripMenuItem
+            // 
+            this.выходToolStripMenuItem.Name = "выходToolStripMenuItem";
+            this.выходToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.выходToolStripMenuItem.Text = "Выход";
+            this.выходToolStripMenuItem.Click += new System.EventHandler(this.выходToolStripMenuItem_Click);
+            // 
+            // стартToolStripMenuItem1
+            // 
+            this.стартToolStripMenuItem1.Name = "стартToolStripMenuItem1";
+            this.стартToolStripMenuItem1.Size = new System.Drawing.Size(180, 22);
+            this.стартToolStripMenuItem1.Text = "Старт";
+            this.стартToolStripMenuItem1.Click += new System.EventHandler(this.стартToolStripMenuItem_Click);
+            // 
+            // стопToolStripMenuItem1
+            // 
+            this.стопToolStripMenuItem1.Name = "стопToolStripMenuItem1";
+            this.стопToolStripMenuItem1.Size = new System.Drawing.Size(180, 22);
+            this.стопToolStripMenuItem1.Text = "Стоп";
+            this.стопToolStripMenuItem1.Click += new System.EventHandler(this.стопToolStripMenuItem_Click);
+            // 
+            // toolStripMenuItem2
+            // 
+            this.toolStripMenuItem2.Name = "toolStripMenuItem2";
+            this.toolStripMenuItem2.Size = new System.Drawing.Size(177, 6);
+            // 
+            // очиститьПолеToolStripMenuItem
+            // 
+            this.очиститьПолеToolStripMenuItem.Name = "очиститьПолеToolStripMenuItem";
+            this.очиститьПолеToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.очиститьПолеToolStripMenuItem.Text = "Очистить поле";
+            this.очиститьПолеToolStripMenuItem.Click += new System.EventHandler(this.отчиститьПолеToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(752, 840);
+            this.ContextMenuStrip = this.contextMenuStrip1;
+            this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.progressBar1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Timer timer1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem игратьToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem стартToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem стопToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem отчиститьПолеToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem выйтиИзИгрыToolStripMenuItem;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem менюToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem игартьToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem стартToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem стопToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripSeparator toolStripMenuItem2;
+        private System.Windows.Forms.ToolStripMenuItem очиститьПолеToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem выходToolStripMenuItem;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,283 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace game_is_live
+{
+    public partial class Form1 : Form
+    {
+        private Panel[,] _panel = new Panel[48, 48];
+        private int[,] _panelBefor = new int[48, 48];
+
+        public Panel panel = new Panel();
+        public int frends = 0;
+        const String G = "Green";
+        public Form1() 
+        {
+            InitializeComponent();
+            
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+
+        }
+
+        private void panel_Click(object sender, EventArgs e)
+        {
+            Panel clickedPanel = (Panel)sender;
+            clickedPanel.BackColor = Color.Green;
+
+            for (int x = 0; x < 36; x++)
+            {
+                for (int y = 0; y < 36; y++)
+                {
+                    if (_panel[x, y].Name == clickedPanel.Name)
+                    {
+                        _panel[x, y].BackColor = Color.Green;
+                        _panelBefor[x, y] = 1;
+                    }
+                }
+            }
+        }
+
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            for (int x = 0; x < 36; x++)
+            {
+                for (int y = 0; y < 36; y++)
+                {
+                    panel.Location = new Point(16 + 20 * x, 16 + 20 * y);               //пример создания панели
+                    panel.Size = new Size(16, 16);
+                    panel.BackColor = Color.White;
+                    panel.Click += new EventHandler(panel_Click);
+                    panel.Name = x + " " + y;
+                    panel.BorderStyle = BorderStyle.Fixed3D;
+                    panel.ContextMenuStrip = contextMenuStrip1;
+                    progressBar1.Value = progressBar1.Value + 1;
+
+                    _panel[x, y] = panel;
+                    _panelBefor[x, y] = 0;
+                    this.Controls.Add(_panel[x, y]);
+                    panel = new Panel();
+
+                }
+            }
+             
+            progressBar1.Value = 0;
+            progressBar1.Enabled = false;
+            progressBar1.Visible = false;
+            button1.Enabled = false;
+            button1.Visible = false;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (button1.Enabled == true)
+            {
+                timer1.Enabled = false;
+                return;
+            }
+            for (int x = 0; x < 35; x++)
+            {
+                for (int y = 0; y < 35; y++)
+                {
+                    switch (y)
+                    {
+                        case 0:
+                        {
+                            if (x == 0)
+                            {
+                                if (_panel[x + 1, y].BackColor.Name == G)     frends++;
+                                if (_panel[x + 1, y + 1].BackColor.Name == G) frends++;
+                                if (_panel[x, y + 1].BackColor.Name == G)     frends++;
+
+                                if ((frends == 3)/* | (frends == 2)*/) _panelBefor[x, y] = 1;
+                                if (((frends == 3) | (frends == 2)) & (_panelBefor[x, y] == 1)) _panelBefor[x, y] = 1;
+                                if ((frends != 3) & (frends != 2) & (_panel[x, y].BackColor.Name == G)) _panelBefor[x,y] = 0;
+                                frends = 0;
+
+                            }
+                            else
+                            {
+                                if (_panel[x - 1, y].BackColor.Name == G)     frends++;
+                                if (_panel[x - 1, y + 1].BackColor.Name == G) frends++;
+                                if (_panel[x + 1, y].BackColor.Name == G)     frends++;
+                                if (_panel[x + 1, y + 1].BackColor.Name == G) frends++;
+                                if (_panel[x, y + 1].BackColor.Name == G)     frends++;
+
+                                if ((frends == 3)/* | (frends == 2)*/) _panelBefor[x, y] = 1;
+                                if (((frends == 3) | (frends == 2)) & (_panelBefor[x, y] == 1)) _panelBefor[x, y] = 1;
+                                if ((frends != 3) & (frends != 2) & (_panel[x, y].BackColor.Name == G)) _panelBefor[x, y] = 0;
+                                frends = 0;
+                            }
+                        }
+                        break;
+
+                        default:
+                        {
+                            if (x == 0)
+                            {
+                                if (_panel[x, y + 1].BackColor.Name == G) frends++;
+                                if (_panel[x, y - 1].BackColor.Name == G) frends++;
+                                if (_panel[x + 1, y].BackColor.Name == G) frends++;
+                                if (_panel[x + 1, y + 1].BackColor.Name == G) frends++;
+                                if (_panel[x + 1, y - 1].BackColor.Name == G) frends++;
+
+                                if ((frends == 3)/* | (frends == 2)*/) _panelBefor[x, y] = 1;
+                                if (((frends == 3) | (frends == 2)) & (_panelBefor[x, y] == 1)) _panelBefor[x, y] = 1;
+                                if ((frends != 3) & (frends != 2) & (_panel[x, y].BackColor.Name == G)) _panelBefor[x, y] = 0;
+                                frends = 0;
+                            }
+                            else
+                            {
+
+                                if (_panel[x, y + 1].BackColor.Name == G) frends++;
+                                if (_panel[x, y - 1].BackColor.Name == G) frends++;
+                                if (_panel[x + 1, y].BackColor.Name == G) frends++;
+                                if (_panel[x - 1, y].BackColor.Name == G) frends++;
+                                if (_panel[x + 1, y + 1].BackColor.Name == G) frends++;
+                                if (_panel[x - 1, y - 1].BackColor.Name == G) frends++;
+                                if (_panel[x + 1, y - 1].BackColor.Name == G) frends++;
+                                if (_panel[x - 1, y + 1].BackColor.Name == G) frends++;
+
+                                if ((frends == 3)/* | (frends == 2)*/) _panelBefor[x, y] = 1;
+                                if (((frends == 3) | (frends == 2)) & (_panelBefor[x, y] == 1)) _panelBefor[x, y] = 1;
+                                if ((frends != 3) & (frends != 2) & (_panel[x, y].BackColor.Name == G)) _panelBefor[x, y] = 0;
+                                frends = 0;
+                            }
+                        }
+                        break;
+                    }
+                }
+            }
+
+            for (int x = 0; x < 36; x++)
+            {
+                for (int y = 0; y < 36; y++)
+                {
+                    if (_panelBefor[x, y] == 1) _panel[x, y].BackColor = Color.Green; else _panel[x, y].BackColor = Color.White;
+                }
+            }
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Space)
+            {
+                timer1.Enabled = !timer1.Enabled;
+                if (timer1.Enabled == true) label2.Text = "enable"; else label2.Text = "disable";
+            }
+            if (e.KeyCode == Keys.Escape)
+            {
+                timer1.Enabled = false;
+                for (int x = 0; x < 36; x++)
+                {
+                    for (int y = 0; y < 36; y++)
+                    {
+                        _panel[x, y].Dispose();
+                        _panel[x, y] = null;
+                        _panelBefor[x, y] = 0;
+                    }
+                }
+
+                GC.Collect();
+                GC.WaitForPendingFinalizers();
+                GC.Collect();
+
+                progressBar1.Value = 0;
+                progressBar1.Enabled = true;
+                progressBar1.Visible = true;
+                button1.Enabled = true;
+                button1.Visible = true;
+            }
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            if (trackBar1.Value > 0) timer1.Interval = trackBar1.Value;
+            label1.Text = timer1.Interval.ToString();
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked == true)
+            {
+                timer1.Enabled = !timer1.Enabled;
+                label2.Text = "enable";
+            }else
+            {
+                timer1.Enabled = !timer1.Enabled;
+                label2.Text = "disable";
+            }
+        }
+
+        private void стопToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = false;
+            label2.Text = "disable";
+        }
+
+        private void стартToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (button1.Enabled == true)
+            {
+                button1_Click(null, null);
+            }
+            else
+            {
+                timer1.Enabled = true;
+                label2.Text = "enable";
+            }
+        }
+
+        private void отчиститьПолеToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (button1.Enabled == false)
+            {
+                for (int x = 0; x < 36; x++)
+                {
+                    for (int y = 0; y < 36; y++)
+                    {
+                        _panel[x, y].BackColor = Color.White;
+                        _panelBefor[x, y] = 0;
+                    }
+                }
+            }
+        }
+
+        private void выйтиИзИгрыToolStripMenuItem_MouseDown(object sender, MouseEventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void backgroundWorker1_DoWork_1(object sender, DoWorkEventArgs e)
+        {
+
+        }
+
+        private void выходToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
